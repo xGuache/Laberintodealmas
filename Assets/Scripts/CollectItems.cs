04018a6b8cefccf4e4a6c0e6fb5e9126f7d3350c
Fernando Vargas <myfer581@hotmail.com>
2023-08-06 02:25:37 -0500

    Nivel 1 cambios
    Falto arreglar se coloco puntos

---
+++ Assets/Scripts/CollectItems.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectItems : MonoBehaviour
+{
+    public AudioSource Collected;
+    private int Cara = 0;
+
+ 
+    [SerializeField] private TextMeshProUGUI CaraText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cara"))
+        {
+            Collected.Play();
+            Destroy(collision.gameObject);
+            Cara++;
+            CaraText.text = "Cara: " + Cara;
+            //Debug.log("Cara; " + Cara);
+        }
+    }
+}
