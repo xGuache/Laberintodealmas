6a6018c465ce9fe4c5836b3d5ddf650cb5bfa716
Fernando Vargas <myfer581@hotmail.com>
2023-08-14 23:48:40 -0500

    Nivel2
    falto configurar los enemigos

--- Assets/Scripts/CollectItems.cs
+++ Assets/Scripts/CollectItems.cs
@@ -7,42 +7,19 @@ public class CollectItems : MonoBehaviour
 {
     public AudioSource Collected;
     private int Cara = 0;
-    //private string Object_Tag;
+
  
     [SerializeField] private TextMeshProUGUI CaraText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Red_Elixir"))
-        {
-            
-            Destroy(collision.gameObject);
-           
-        }
-
         if (collision.gameObject.CompareTag("Cara"))
         {
-            Destroy(collision.gameObject);
             Collected.Play();
+            Destroy(collision.gameObject);
             Cara++;
+            //Debug.Lod("cara: " + Cara);
             CaraText.text = "Cara: " + Cara;
-            //Debug.log("Cara; " + Cara);
-           
         }
-
-        /*switch (collision.gameObject.CompareTag(Object_Tag))
-        {
-            case "Red_Elixir":
-                Destroy(collision.gameObject);
-                break;
-            case "Cara":
-                Destroy(collision.gameObject);
-                break;
-            default:
-                // code block
-                break;
-        }*/
-
     }
-
- }
+}
